e1faff953f6c06c1dbb731f234a958fa83a49aff
fqcoming <fuqiang512215@gmail.com>
2023-06-25 17:36:24 +0000

    ok

---
+++ mysql/test_connpool.cpp
@@ -0,0 +1,58 @@
+
+#include "connpool.hpp"
+
+#define ENABLE_CONNPOOL     0
+
+
+
+
+
+
+
+int main(int argc, char *argv[]) {
+
+
+
+#if ENABLE_CONNPOOL
+
+
+#else
+    Connection conn;
+    char sql[1024] = {0};
+    sprintf(sql, "insert into user(name, age, sex) values('%s', %d, '%s')", "John", 24, "male");
+
+    conn.connect("127.0.0.1", 3306, "root", "123456", "chat");
+    conn.update(sql);
+#endif
+
+    return 0;
+}
+
+
+
+
+// mysql -u root -p 123456
+/****************************
+    DROP TABLE chat.user;
+    CREATE TABLE chat.user (
+        name VARCHAR(255),
+        age INT,
+        sex VARCHAR(10)
+    );
+****************************/
+// g++ -o test_connpool test_connpool.cpp -lmysqlclient
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
